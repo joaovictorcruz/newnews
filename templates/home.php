549b914d665a5cfee2a7ee1b06f768aa638fddef
João Victor <elietejvc@hotmail.com>
2024-11-30 01:22:31 -0300

    finalizando css destaques

--- templates/home.php
+++ templates/home.php
@@ -1,33 +1,38 @@
 
-    <main class="container mt-4 destaques">
-        <h1>Destaques</h1>
-        <div class="row">
-            <div class="col-md-8 col-12 mb-4" id="noticia-destaque">
-                <div class="card">
-                    <img src="https://via.placeholder.com/800x400/6a0dad/ffffff?text=Destaque" class="card-img-top" alt="Notícia em destaque">
-                    <div class="card-body">
-                        <h5 class="card-title">"Supernatural" ultrapassa 100 Milhões de streams no Spotify</h5>
+<main class="container mt-4 destaques">
+    <h1 class="titulo-destaques">Destaques</h1>
+    <div class="destaques-container">
+        <div class="destaque">
+            <div class="destaque-card">
+                <img src="./src/1.png" class="destaque-img" alt="Notícia em destaque">
+                <div class="destaque-conteudo">
+                    <h5 class="destaque-titulo">NewJeans oficializa o fim de seu contrato com ADOR</h5>
+                </div>
+            </div>
+        </div>
+        <div class="secundarias">
+            <div class="secundaria">
+                <div class="secundaria-card">
+                    <img src="./src/2.png" class="secundaria-img" alt="Notícia secundária">
+                    <div class="secundaria-conteudo">
+                        <h5 class="secundaria-titulo">NewJeans aciona 'Coletiva de Imprensa de Emergência' a respeito de seu contrato com ADOR</h5>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 col-12 d-flex flex-column">
-                <div class="card mb-4" id="noticia-secundaria-1">
-                    <img src="https://via.placeholder.com/400x200/6a0dad/ffffff?text=Not%C3%ADcia+Secund%C3%A1ria+1" class="card-img-top" alt="Notícia secundária">
-                    <div class="card-body">
-                        <h5 class="card-title">NewJeans como embaixadores da Indomie</h5>
+            <div class="secundaria">
+                <div class="secundaria-card">
+                    <img src="./src/3.png" class="secundaria-img" alt="Notícia secundária">
+                    <div class="secundaria-conteudo">
+                        <h5 class="secundaria-titulo">Daniele marca presença no tapete vermelho do evento da Celine</h5>
                     </div>
                 </div>
-                <div class="card" id="noticia-secundaria-2">
-                    <img src="https://via.placeholder.com/400x200/6a0dad/ffffff?text=Not%C3%ADcia+Secund%C3%A1ria+2" class="card-img-top" alt="Notícia secundária">
-                    <div class="card-body">
-                        <h5 class="card-title">ADOR revela que Harin não fará provas coreanas</h5>
-                    </div>
-                </div>
-            </div> 
+            </div>
         </div>
-    </main>
+    </div>
+</main>
+
     <hr>
-    <main class="container mt-4">
+    <main class="container mt-4" class="noticias-home">
     <h1>Notícias</h1>
     <?php require './controller/exibeNoticias.php'?>
     </main>
