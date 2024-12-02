e5c57bd60207dffe7ea20805a9be6ec2812306a5
João Victor <elietejvc@hotmail.com>
2024-12-01 21:31:42 -0300

    finalizando css home page

--- templates/home.php
+++ templates/home.php
@@ -13,17 +13,17 @@
         <div class="secundarias">
             <div class="secundaria">
                 <div class="secundaria-card">
-                    <img src="<?=$secundaria1Imagem?>" class="secundaria-img" alt="Notícia secundária">
+                    <img class="secundaria-img" src="<?=$secundaria1Imagem?>"  alt="Notícia secundária">
                     <div class="secundaria-conteudo">
-                    <h5 class="destaque-titulo card-title"><a href="?pagina=noticia&&noticiaId=<?=$secundaria1Id?>"><?=$secundaria1Titulo?></a></h5>
+                    <h5 class="secundaria-titulo card-title"><a href="?pagina=noticia&&noticiaId=<?=$secundaria1Id?>"><?=$secundaria1Titulo?></a></h5>
                     </div>
                 </div>
             </div>
             <div class="secundaria">
                 <div class="secundaria-card">
-                    <img src="<?=$secundaria2Imagem?>" class="secundaria-img" alt="Notícia secundária">
+                    <img class="secundaria-img" src="<?=$secundaria2Imagem?>"  alt="Notícia secundária">
                     <div class="secundaria-conteudo">
-                    <h5 class="destaque-titulo card-title"><a href="?pagina=noticia&&noticiaId=<?=$secundaria2Id?>"><?=$secundaria2Titulo?></a></h5>
+                    <h5 class="secundaria-titulo card-title"><a href="?pagina=noticia&&noticiaId=<?=$secundaria2Id?>"><?=$secundaria2Titulo?></a></h5>
                     </div>
                 </div>
             </div>
